a632492d5863acf9afa347dea223c5d7fff84705
Thái Dân <103986347+thaidan13@users.noreply.github.com>
2022-12-22 17:55:51 +0700

    first commit

---
+++ DongHoBamGio/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace DongHoBamGio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnstart = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(72, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00:00:00:00";
+            // 
+            // btnstart
+            // 
+            this.btnstart.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(204)))), ((int)(((byte)(153)))));
+            this.btnstart.Appearance.BackColor2 = System.Drawing.Color.White;
+            this.btnstart.Appearance.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnstart.Appearance.Options.UseBackColor = true;
+            this.btnstart.Appearance.Options.UseFont = true;
+            this.btnstart.Location = new System.Drawing.Point(77, 217);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(127, 45);
+            this.btnstart.TabIndex = 1;
+            this.btnstart.Text = "Start";
+            this.btnstart.Click += new System.EventHandler(this.btnstart_Click);
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(85)))), ((int)(((byte)(92)))));
+            this.simpleButton1.Appearance.BackColor2 = System.Drawing.Color.White;
+            this.simpleButton1.Appearance.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.simpleButton1.Appearance.Options.UseBackColor = true;
+            this.simpleButton1.Appearance.Options.UseFont = true;
+            this.simpleButton1.Location = new System.Drawing.Point(292, 217);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(127, 45);
+            this.simpleButton1.TabIndex = 2;
+            this.simpleButton1.Text = "Stop";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(129)))), ((int)(((byte)(198)))));
+            this.simpleButton2.Appearance.BackColor2 = System.Drawing.Color.White;
+            this.simpleButton2.Appearance.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.simpleButton2.Appearance.Options.UseBackColor = true;
+            this.simpleButton2.Appearance.Options.UseFont = true;
+            this.simpleButton2.Location = new System.Drawing.Point(508, 217);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(127, 45);
+            this.simpleButton2.TabIndex = 3;
+            this.simpleButton2.Text = "Reset";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // Form1
+            // 
+            this.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(38)))), ((int)(((byte)(48)))));
+            this.Appearance.Options.UseBackColor = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 325);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.btnstart);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đồng hồ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private DevExpress.XtraEditors.SimpleButton btnstart;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+    }
+}
+
---
+++ DongHoBamGio/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace DongHoBamGio
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        System.Timers.Timer timer;
+        int h, m, s, ms;
+
+        PrivateFontCollection modernFont = new PrivateFontCollection();
+
+        
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            h = 0;
+            m = 0;
+            s = 0;
+            ms = 0;
+            timer.Stop();
+            
+            label1.Font = new Font(modernFont.Families[0], 60);
+            label1.Text = "00:00:00:00";
+        }
+
+        private void btnstart_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            modernFont.AddFontFile(@"C:\Users\Admin\Documents\WebAPS\LCDM2B__.ttf");
+
+            label1.Font = new Font(modernFont.Families[0], 60);
+            btnstart.Font = new Font(modernFont.Families[0], 20);
+            simpleButton1.Font = new Font(modernFont.Families[0], 20);
+            simpleButton2.Font = new Font(modernFont.Families[0], 20);
+
+            timer = new System.Timers.Timer();
+            timer.Interval = 1;
+            timer.Elapsed += OnTimeEvent;
+        }
+
+        private void OnTimeEvent(object sender, ElapsedEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                ms += 1;
+                if(ms == 100)
+                {
+                    ms = 0;
+                    s += 1;
+                }
+                if(s == 60)
+                {
+                    ms = 0;
+                    s += 1;
+                }
+                if(m == 60)
+                {
+                    m = 0;
+                    h += 1;
+                }
+
+                label1.Text = string.Format("{0}:{1}:{2}:{3}", h.ToString().ToString().PadLeft(2, '0'), m.ToString().ToString().PadLeft(2, '0'), s.ToString().ToString().PadLeft(2, '0'), ms.ToString().ToString().PadLeft(2, '0'));
+
+            }));
+        }
+    }
+}
